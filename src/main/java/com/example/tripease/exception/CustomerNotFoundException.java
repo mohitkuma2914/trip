eda03614339a2889e7e283b0fb0e54ae1cc13bbd
mohitkuma2914 <mohitkuma2914@gmail.com>
2026-02-07 17:04:13 +0530

    first

--- src/main/java/com/example/tripease/exception/CustomerNotFoundException.java
+++ src/main/java/com/example/tripease/exception/CustomerNotFoundException.java
@@ -3,6 +3,8 @@ package com.example.tripease.exception;
 public class CustomerNotFoundException extends RuntimeException{
     public CustomerNotFoundException(String message){
         super(message);
+
+//mohit
     }
 
 }
